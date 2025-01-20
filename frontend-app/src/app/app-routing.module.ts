d6dcbcbe37fdc03e2c69cfc3dd4814e38cc29a4b
Nourhen K <120370791+nourhen369@users.noreply.github.com>
2025-01-20 01:27:06 +0100

    top communities component

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './feed/posts/post-list/post-list.component';
 import { PostDetailsComponent } from './feed/posts/post-details/post-details.component';
 import { CreateCommunityComponent } from './feed/communities/create-community/create-community.component';
+import { TopCommunitiesComponent } from './feed/communities/top-communities/top-communities.component';
+import { CommunityDetailsComponent } from './feed/communities/community-details/community-details.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'post/:id', component: PostDetailsComponent },
   { path: 'community', component: CreateCommunityComponent },
+  { path: 'communities', component: TopCommunitiesComponent },
+  { path: 'community/:name', component: CommunityDetailsComponent },
 ];
 
 @NgModule({
