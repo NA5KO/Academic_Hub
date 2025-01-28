53159b244ea481a67c3e13c2f47fb423c5d012d1
Amine Yahya <amine101yahya@gmail.com>
2025-01-28 11:18:19 +0100

    Jwt auth working (PhotoUrl)

--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ProfileModule } from './profile/profile.module';
 import { LoginComponent } from './auth/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GoogleLoginProvider, SocialAuthServiceConfig ,SocialLoginModule  } from 'angularx-social-login';
 
 @NgModule({
   declarations: [
@@ -28,9 +30,24 @@ import { SignupComponent } from './auth/signup/signup.component';
     RightMenuModule,
     PostsModule,
     ProfileModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('491160602747-ovdftgsvteebotmki95c6r068mfk4op9.apps.googleusercontent.com')
+          },
+        ]
+      } as SocialAuthServiceConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
