55aa91c7af93ba62fa798373f878f467523acb08
Nourhen K <120370791+nourhen369@users.noreply.github.com>
2025-02-07 19:50:33 +0100

    fixed an issue for the community's page

--- frontend-app/src/app/feed/communities/community-header/community-header.component.ts
+++ frontend-app/src/app/feed/communities/community-header/community-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommunitiesService } from 'src/services/communities.service';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/services/auth.service';
@@ -8,22 +8,10 @@ import { AuthService } from 'src/services/auth.service';
   templateUrl: './community-header.component.html',
   styleUrls: ['./community-header.component.css', '../../posts/post-list/post-list.component.css'],
   standalone: false,
-
 })
 export class CommunityHeaderComponent implements OnInit {
-
   isFollowing: boolean = false;
   userId: string = this.authService.checkAuthToken() ? this.authService.getUserIdFromLocalStorage() : '';
-
-  // check this later
-  isAdmin: boolean = false;
-
-  constructor(
-    private communityService: CommunitiesService, 
-    private route: ActivatedRoute,
-    private authService: AuthService
-  ) {
-  }
 
   community = {
     id: '',
@@ -36,44 +24,48 @@ export class CommunityHeaderComponent implements OnInit {
     posts: []
   };
 
+  constructor(
+    private communityService: CommunitiesService,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    const communityName = this.route.snapshot.paramMap.get('name');
-    if(communityName){
-      this.communityService.getCommunity(communityName).subscribe((data) => {
-        this.community = data;
-        this.checkIfFollowing();
+    // Listen for route changes
+    this.route.paramMap.subscribe(params => {
+      const communityName = params.get('name');
+      if (communityName) {
+        this.loadCommunity(communityName);
+      }
+    });
+  }
+
+  loadCommunity(communityName: string) {
+    this.communityService.getCommunity(communityName).subscribe((data) => {
+      this.community = data;
+      this.checkIfFollowing();
+    });
+  }
+
+  checkIfFollowing() {
+    if (this.community.id) {
+      this.communityService.isFollowing(this.userId, this.community.id).subscribe((isFollowing) => {
+        this.isFollowing = isFollowing;
       });
     }
-    // this.isAdmin = this.authService.isAdmin();
-    }
+  }
 
-    checkIfFollowing() {
-      if (this.community.id) {
-        this.communityService.isFollowing(this.userId, this.community.id).subscribe((isFollowing) => {
-          this.isFollowing = isFollowing;
-        });
-      }
+  followCommunity() {
+    if (!this.isFollowing) {
+      this.communityService.followCommunity(this.userId, this.community.id).subscribe(() => {
+        this.isFollowing = true;
+        this.community.followersCount++;
+      });
+    } else {
+      this.communityService.unfollowCommunity(this.userId, this.community.id).subscribe(() => {
+        this.isFollowing = false;
+        this.community.followersCount--;
+      });
     }
-
-    followCommunity() {
-      if (!this.isFollowing) {
-        this.communityService.followCommunity(this.userId, this.community.id).subscribe(() => {
-          this.isFollowing = true;
-          this.community.followersCount++;
-        });
-      } else {
-        this.communityService.unfollowCommunity(this.userId, this.community.id).subscribe(() => {
-          this.isFollowing = false;
-          this.community.followersCount--;
-        });
-      }
-    }
-  
-
-    // mch mawjouda fl front
-    editCommunity() {
-      console.log('Editing community');
-      // Handle community edit logic here
-    }
+  }
 }
